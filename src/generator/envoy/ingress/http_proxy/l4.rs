fd0cae7d14ad78b18c9634c15db88c2882c1074a
Kun Lai <laikun@linux.alibaba.com>
2024-08-07 10:27:04 +0000

    ingress/http_proxy/l4: fix broken dynamic_forward_proxy
    Signed-off-by: Kun Lai <laikun@linux.alibaba.com>

--- src/generator/envoy/ingress/http_proxy/l4.rs
+++ src/generator/envoy/ingress/http_proxy/l4.rs
@@ -65,21 +65,16 @@ pub fn gen(
                   cluster: tng_ingress{id}_wrap_in_h2_tls_upstream
             {}
           http_filters:
-          - name: envoy.filters.http.dynamic_forward_proxy
+          - name: envoy.filters.http.set_filter_state
             typed_config:
-              "@type": type.googleapis.com/envoy.extensions.filters.http.dynamic_forward_proxy.v3.FilterConfig
-              dns_cache_config:
-                name: dynamic_forward_proxy_cache_config
-                dns_lookup_family: V4_ONLY
-                typed_dns_resolver_config:
-                  name: envoy.network.dns_resolver.cares
-                  typed_config:
-                    "@type": type.googleapis.com/envoy.extensions.network.dns_resolver.cares.v3.CaresDnsResolverConfig
-                    resolvers:
-                    use_resolvers_as_fallback: true
-                    dns_resolver_options:
-                      use_tcp_for_dns_lookups: false
-                      no_default_search_domain: true
+              "@type": type.googleapis.com/envoy.extensions.filters.http.set_filter_state.v3.Config
+              on_request_headers:
+              - object_key: envoy.upstream.dynamic_host
+                factory_key: envoy.string
+                format_string:
+                  text_format_source:
+                    inline_string: "%REQ(:AUTHORITY)%"
+                shared_with_upstream: TRANSITIVE
           - name: envoy.filters.http.router
             typed_config:
               "@type": type.googleapis.com/envoy.extensions.filters.http.router.v3.Router
@@ -171,6 +166,23 @@ pub fn gen(
     internal_listener: {{}}
     filter_chains:
     - filters:
+      # Here we use sni_dynamic_forward_proxy to consume "envoy.upstream.dynamic_host"
+      - name: envoy.filters.network.sni_dynamic_forward_proxy
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.filters.network.sni_dynamic_forward_proxy.v3.FilterConfig
+          port_value: 80 # Set the default port when the host (:AUTHORITY header) has no port. See https://github.com/envoyproxy/envoy/blob/7976424646e63daa384ef51fdb9ac40cb6cb6d98/source/extensions/common/dynamic_forward_proxy/dns_cache.h#L34
+          dns_cache_config:
+            name: dynamic_forward_proxy_cache_config
+            dns_lookup_family: V4_ONLY
+            typed_dns_resolver_config:
+              name: envoy.network.dns_resolver.cares
+              typed_config:
+                "@type": type.googleapis.com/envoy.extensions.network.dns_resolver.cares.v3.CaresDnsResolverConfig
+                resolvers:
+                use_resolvers_as_fallback: true
+                dns_resolver_options:
+                  use_tcp_for_dns_lookups: false
+                  no_default_search_domain: true
       - name: envoy.filters.network.tcp_proxy
         typed_config:
           "@type": type.googleapis.com/envoy.extensions.filters.network.tcp_proxy.v3.TcpProxy
@@ -270,12 +282,69 @@ pub fn gen(
     Ok((listeners, clusters))
 }
 
-fn add_fallback_route(clusters: &mut Vec<String>, _listeners: &mut Vec<String>, id: usize) {
+fn add_fallback_route(clusters: &mut Vec<String>, listeners: &mut Vec<String>, id: usize) {
     // Add a cluster for forwording traffics to upstream directly, without going through TNG tunnel.
     {
         clusters.push(format!(
       r#"
   - name: tng_ingress{id}_direct_entry_upstream
+    load_assignment:
+      cluster_name: tng_ingress{id}_direct_entry_upstream
+      endpoints:
+      - lb_endpoints:
+        - endpoint:
+            address:
+              envoy_internal_address:
+                server_listener_name: tng_ingress{id}_direct_entry
+    transport_socket:
+      name: envoy.transport_sockets.internal_upstream
+      typed_config:
+        "@type": type.googleapis.com/envoy.extensions.transport_sockets.internal_upstream.v3.InternalUpstreamTransport
+        transport_socket:
+          name: envoy.transport_sockets.raw_buffer
+          typed_config:
+            "@type": type.googleapis.com/envoy.extensions.transport_sockets.raw_buffer.v3.RawBuffer
+    "#
+        ));
+    }
+
+    {
+        listeners.push(format!(
+            r#"
+  - name: tng_ingress{id}_direct_entry
+    internal_listener: {{}}
+    filter_chains:
+    - filters:
+      # Here we use sni_dynamic_forward_proxy to consume "envoy.upstream.dynamic_host"
+      - name: envoy.filters.network.sni_dynamic_forward_proxy
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.filters.network.sni_dynamic_forward_proxy.v3.FilterConfig
+          port_value: 80 # Set the default port when the host (:AUTHORITY header) has no port. See https://github.com/envoyproxy/envoy/blob/7976424646e63daa384ef51fdb9ac40cb6cb6d98/source/extensions/common/dynamic_forward_proxy/dns_cache.h#L34
+          dns_cache_config:
+            name: dynamic_forward_proxy_cache_config
+            dns_lookup_family: V4_ONLY
+            typed_dns_resolver_config:
+              name: envoy.network.dns_resolver.cares
+              typed_config:
+                "@type": type.googleapis.com/envoy.extensions.network.dns_resolver.cares.v3.CaresDnsResolverConfig
+                resolvers:
+                use_resolvers_as_fallback: true
+                dns_resolver_options:
+                  use_tcp_for_dns_lookups: false
+                  no_default_search_domain: true
+      - name: envoy.filters.network.tcp_proxy
+        typed_config:
+          "@type": type.googleapis.com/envoy.extensions.filters.network.tcp_proxy.v3.TcpProxy
+          stat_prefix: tng_ingress{id}_direct_entry
+          cluster: tng_ingress{id}_direct_upstream
+    "#
+        ));
+    }
+
+    {
+        clusters.push(format!(
+            r#"
+  - name: tng_ingress{id}_direct_upstream
     lb_policy: CLUSTER_PROVIDED
     cluster_type:
       name: envoy.clusters.dynamic_forward_proxy
@@ -293,14 +362,6 @@ fn add_fallback_route(clusters: &mut Vec<String>, _listeners: &mut Vec<String>, 
               dns_resolver_options:
                 use_tcp_for_dns_lookups: false
                 no_default_search_domain: true
-    transport_socket:
-      name: envoy.transport_sockets.internal_upstream
-      typed_config:
-        "@type": type.googleapis.com/envoy.extensions.transport_sockets.internal_upstream.v3.InternalUpstreamTransport
-        transport_socket:
-          name: envoy.transport_sockets.raw_buffer
-          typed_config:
-            "@type": type.googleapis.com/envoy.extensions.transport_sockets.raw_buffer.v3.RawBuffer
   "#
       ));
     }
