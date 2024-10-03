9018a81c79ebfedc18c95fb257989132cefb27a9
Kun Lai <laikun@linux.alibaba.com>
2024-10-03 14:46:50 +0000

    native: change client pool from mutex to rwlock
    Signed-off-by: Kun Lai <laikun@linux.alibaba.com>

--- src/tunnel/ingress/core/client/stream/manager.rs
+++ src/tunnel/ingress/core/client/stream/manager.rs
@@ -14,7 +14,7 @@ use hyper_util::{
     client::legacy::Client,
     rt::{TokioExecutor, TokioIo},
 };
-use tokio::{net::TcpStream, sync::Mutex};
+use tokio::{net::TcpStream, sync::RwLock};
 use tokio_rustls::rustls::{ClientConfig, RootCertStore};
 
 use crate::tunnel::ingress::core::{client::stream::verifier::NoRaCertVerifier, TngEndpoint};
@@ -24,8 +24,7 @@ type PoolKey = TngEndpoint;
 type ClientType = Client<HttpsConnector<HttpConnector>, axum::body::Body>;
 
 pub struct StreamManager {
-    // TODO: Optimize this to RWLock
-    pool: Mutex<HashMap<PoolKey, ClientType>>,
+    pool: RwLock<HashMap<PoolKey, ClientType>>,
 }
 
 impl StreamManager {
@@ -36,33 +35,46 @@ impl StreamManager {
     }
 
     async fn get_client(&self, endpoint: &TngEndpoint) -> Result<ClientType> {
-        let mut lock = self.pool.lock().await;
+        // Try to get the client from pool
+        let client = {
+            let read = self.pool.read().await;
+            read.get(endpoint).map(|c| c.clone())
+        };
 
-        let client = match lock.get(endpoint) {
-            Some(c) => c.clone(),
+        let client = match client {
+            Some(c) => c,
             None => {
-                tracing::info!("Creating client for '{}'", endpoint);
-                let http_connector = HttpConnector::new(endpoint.clone());
+                // If client not exist then we need to create one
+                let mut write = self.pool.write().await;
+                // Check if client has been created by other "thread"
+                match write.get(endpoint) {
+                    Some(c) => c.clone(),
+                    None => {
+                        tracing::info!("Creating client for '{}'", endpoint);
+                        let http_connector = HttpConnector::new(endpoint.clone());
 
-                // TLS client config using the custom CA store for lookups
-                let mut tls =
-                    ClientConfig::builder_with_protocol_versions(&[&rustls::version::TLS13])
+                        // TLS client config using the custom CA store for lookups
+                        let mut tls = ClientConfig::builder_with_protocol_versions(&[
+                            &rustls::version::TLS13,
+                        ])
                         .with_root_certificates(RootCertStore::empty())
                         .with_no_client_auth(); // TODO: use with_client_cert_resolver() to provide client cert
-                tls.dangerous()
-                    .set_certificate_verifier(Arc::new(NoRaCertVerifier::new()?));
+                        tls.dangerous()
+                            .set_certificate_verifier(Arc::new(NoRaCertVerifier::new()?));
 
-                // Prepare the HTTPS connector
-                let https_connector = hyper_rustls::HttpsConnectorBuilder::new()
-                    .with_tls_config(tls)
-                    .https_only() // TODO: support returning notification message on non rats-tls request with https_or_http()
-                    .enable_http2()
-                    .wrap_connector(http_connector);
+                        // Prepare the HTTPS connector
+                        let https_connector = hyper_rustls::HttpsConnectorBuilder::new()
+                            .with_tls_config(tls)
+                            .https_only() // TODO: support returning notification message on non rats-tls request with https_or_http()
+                            .enable_http2()
+                            .wrap_connector(http_connector);
 
-                // Build the hyper client from the HTTPS connector.
-                let client = Client::builder(TokioExecutor::new()).build(https_connector);
-                lock.insert(endpoint.to_owned(), client.clone());
-                client
+                        // Build the hyper client from the HTTPS connector.
+                        let client = Client::builder(TokioExecutor::new()).build(https_connector);
+                        write.insert(endpoint.to_owned(), client.clone());
+                        client
+                    }
+                }
             }
         };
 
