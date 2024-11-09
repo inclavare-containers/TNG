9c697dc4cee5b3cb750eb5d2106187268de58a56
Kun Lai <laikun@linux.alibaba.com>
2024-11-09 17:56:26 +0000

    cli: add git commit id to --version output
    Signed-off-by: Kun Lai <laikun@linux.alibaba.com>

--- src/bin/tng/cli.rs
+++ src/bin/tng/cli.rs
@@ -2,8 +2,11 @@ use std::path::PathBuf;
 
 use clap::{arg, Parser};
 
+use crate::build::CLAP_LONG_VERSION;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
+#[clap(long_version = CLAP_LONG_VERSION)]
 pub enum Args {
     #[command(name = "launch")]
     Launch(LaunchOptions),
