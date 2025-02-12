fc3aec75042919d1a6de5b0978ea2eaf887d5e56
Kun Lai <laikun@linux.alibaba.com>
2025-02-12 08:48:12 +0000

    cert_manager: fix error in parsing privkey generated by rats-rs
    Signed-off-by: Kun Lai <laikun@linux.alibaba.com>

--- src/tunnel/utils/cert_manager.rs
+++ src/tunnel/utils/cert_manager.rs
@@ -4,9 +4,9 @@ use rats_cert::{
     crypto::{AsymmetricAlgo, HashAlgo},
     tee::coco::attester::CocoAttester,
 };
-use rustls::pki_types::pem::{PemObject, SectionKind};
 use std::sync::{Arc, Mutex};
 use tokio_graceful::ShutdownGuard;
+use tracing::{Instrument, Span};
 
 use crate::config::ra::AttestArgs;
 
@@ -38,37 +38,52 @@ impl CertManager {
         // TODO: terminate the task when CertManager is dropped
         {
             let latest_cert = latest_cert.clone();
-            shutdown_guard.spawn_task_fn(|shutdown_guard| async move {
-                loop {
-                    let attest_args = attest_args.clone();
-                    let latest_cert = latest_cert.clone();
-                    let join_handle =
-                        tokio::task::spawn_blocking(move || -> Result<_, anyhow::Error> {
-                            let certed_key: Arc<rustls::sign::CertifiedKey> =
-                                Self::update_cert_blocking(attest_args)?;
-                            *latest_cert.lock().unwrap() = certed_key;
-                            Ok(())
-                        });
+            let span = Span::current();
+            shutdown_guard.spawn_task_fn(|shutdown_guard| {
+                async move {
+                    let res = async {
+                        loop {
+                            // Update every hour
+                            tokio::select! {
+                                _ = shutdown_guard.cancelled() => {
+                                    break;
+                                }
+                                _ = tokio::time::sleep(tokio::time::Duration::from_secs(
+                                    CERT_UPDATE_INTERVAL_SECOND as u64,
+                                )) => {}
+                            }
 
-                    let abort_handle = join_handle.abort_handle();
-                    tokio::select! {
-                        _ = shutdown_guard.cancelled() => {
-                            abort_handle.abort();
-                            break;
+                            let attest_args = attest_args.clone();
+                            let latest_cert = latest_cert.clone();
+                            let join_handle =
+                                tokio::task::spawn_blocking(move || -> Result<_, anyhow::Error> {
+                                    let certed_key: Arc<rustls::sign::CertifiedKey> =
+                                        Self::update_cert_blocking(attest_args)?;
+                                    *latest_cert.lock().unwrap() = certed_key;
+                                    Ok(())
+                                });
+
+                            let abort_handle = join_handle.abort_handle();
+                            tokio::select! {
+                                _ = shutdown_guard.cancelled() => {
+                                    abort_handle.abort();
+                                    break;
+                                }
+                                result = join_handle => {
+                                    result??;
+                                }
+                            }
                         }
-                        result = join_handle => {
-                            result??;
-                        }
+                        #[allow(unreachable_code)]
+                        Ok::<(), anyhow::Error>(())
                     }
-
-                    // Update every hour
-                    tokio::time::sleep(tokio::time::Duration::from_secs(
-                        CERT_UPDATE_INTERVAL_SECOND as u64,
-                    ))
                     .await;
+
+                    if let Err(e) = res {
+                        tracing::error!("Failed to update cert: {:#}", e);
+                    }
                 }
-                #[allow(unreachable_code)]
-                Ok::<(), anyhow::Error>(())
+                .instrument(span)
             });
         }
 
@@ -76,9 +91,15 @@ impl CertManager {
     }
 
     fn update_cert_blocking(attest_args: AttestArgs) -> Result<Arc<rustls::sign::CertifiedKey>> {
+        let timeout_sec = CREATE_CERT_TIMEOUT_SECOND as i64;
+        tracing::trace!(
+            aa_addr = &attest_args.aa_addr,
+            timeout_sec,
+            "Generate new cert with rats-rs"
+        );
         let coco_attester = CocoAttester::new_with_timeout_nano(
             &attest_args.aa_addr,
-            (CREATE_CERT_TIMEOUT_SECOND as i64) * 1000 * 1000 * 1000,
+            timeout_sec * 1000 * 1000 * 1000,
         )?;
         let cert = CertBuilder::new(coco_attester, HashAlgo::Sha256)
             .with_subject("CN=TNG,O=Inclavare Containers")
@@ -86,17 +107,19 @@ impl CertManager {
 
         let der_cert = cert.cert_to_der()?;
         let privkey = cert.private_key().to_pkcs8_pem()?;
+        tracing::trace!(
+            cert = cert.cert_to_pem()?,
+            timeout_sec,
+            "Generated new cert"
+        );
+
         let crypto_provider = rustls::crypto::CryptoProvider::get_default()
             .context("rustls crypto provider not installed")?;
         let certified_key = rustls::sign::CertifiedKey::new(
             vec![rustls::pki_types::CertificateDer::from(der_cert)],
-            crypto_provider.key_provider.load_private_key(
-                rustls::pki_types::PrivateKeyDer::from_pem(
-                    SectionKind::PrivateKey,
-                    privkey.as_bytes().into(),
-                )
-                .context("bug")?,
-            )?,
+            crypto_provider
+                .key_provider
+                .load_private_key(rustls_pemfile::private_key(&mut privkey.as_bytes())?.unwrap())?,
         );
         Ok(Arc::new(certified_key))
     }
