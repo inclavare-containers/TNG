c073d27c30acd84e1154b279a07cc708d8c45f0f
Kun Lai <laikun@linux.alibaba.com>
2025-10-22 08:14:49 +0000

    refactor(tng-wasm): change console log to no color
    Signed-off-by: Kun Lai <laikun@linux.alibaba.com>

--- tng-wasm/src/lib.rs
+++ tng-wasm/src/lib.rs
@@ -14,7 +14,7 @@ use tng::{
 
 use tracing_subscriber::layer::SubscriberExt as _;
 use tracing_subscriber::Layer;
-use tracing_wasm::{WASMLayer, WASMLayerConfig};
+use tracing_wasm::{WASMLayer, WASMLayerConfigBuilder};
 use wasm_bindgen::prelude::*;
 
 pub mod fetch;
@@ -25,12 +25,13 @@ pub fn init_tng() {
     // This is not needed for tracing_wasm to work, but it is a common tool for getting proper error line numbers for panics.
     console_error_panic_hook::set_once();
 
+    let wasm_layer_config = WASMLayerConfigBuilder::new()
+        .set_console_config(tracing_wasm::ConsoleConfig::ReportWithoutConsoleColor)
+        .build();
     tracing::subscriber::set_global_default(tracing_subscriber::registry().with(
-        WASMLayer::new(WASMLayerConfig::default()).with_filter(
-            Into::<tracing_subscriber::EnvFilter>::into(
-                "info,tokio_graceful=off,rats_cert=debug,tng=debug",
-            ),
-        ),
+        WASMLayer::new(wasm_layer_config).with_filter(Into::<tracing_subscriber::EnvFilter>::into(
+            "info,tokio_graceful=off,rats_cert=debug,tng=debug",
+        )),
     ))
     .expect("failed to set tng default global tracing subscriber");
 
